902299a816b79ceeb7d5fde029eb545daf5bef48
Victor Avila <avilavictor1570@gmail.com>
2020-07-25 02:29:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,9 @@ class Song
     genre_count = {}
     @@genres. each do |genre|
       if genre_count[genre]
-        genre_count[genre] += 1
+      genre_count[genre] += 1
       else
-        genre_count[genre] = 1
+      genre_count[genre] = 1
       end
     end
     genre_count
@@ -42,9 +42,9 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-        artist_count[artist] += 1
+      artist_count[artist] += 1
       else
-        artist_count[artist] = 1
+      artist_count[artist] = 1
       end
     end
     artist_count
