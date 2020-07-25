474465c9128beae0ef57e3b08ab2e9b9aa8c1754
Victor Avila <avilavictor1570@gmail.com>
2020-07-25 02:50:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  @@song_count = 0
+  @@count = 0
   @@genres = []
   @@artists = []
   
@@ -10,13 +10,13 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@song_count +=1
+    @@count +=1
     @@genres << @genre
     @@artists << @artist
   end
   
   def self.count
-    @@song_count
+    @@count
   end
   
   def self.genres
